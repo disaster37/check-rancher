83991045c351a21f5af5b941f33daff9c20b0c57
Sebastien Langoureaux <sebastien.langoureaux@sihm.fr>
2018-06-28 09:02:21 +0000

    Add project name (environment) on each check

--- service/monitoring/host.go
+++ service/monitoring/host.go
@@ -1,71 +1,62 @@
 package monitoringService
 
 import (
-	"github.com/disaster37/check-rancher/model/monitoring"
-	"github.com/disaster37/check-rancher/model/project"
 	"fmt"
+	"github.com/disaster37/check-rancher/model/monitoring"
 	"github.com/pkg/errors"
+	rancherClient "github.com/rancher/go-rancher/v2"
 	log "github.com/sirupsen/logrus"
 )
 
-// Permit to check the project's hosts state from Rancher project data
-func CheckHostsProject(project *modelProject.Project) (*modelMonitoring.Monitoring, error) {
+// Permit to check the hosts state
+func CheckHosts(hosts []rancherClient.Host) (*modelMonitoring.Monitoring, error) {
 
-	if project == nil {
-		return nil, errors.New("Project can't be null")
+	if hosts == nil {
+		return nil, errors.New("Hosts can't be null")
 	}
 
-	log.Debugf("Check state for hosts's project %s", project.Project().Name)
+	log.Debugf("Check state for hosts")
 
 	monitoringData := modelMonitoring.NewMonitoring()
-	projectName := project.Project().Name
 
-	// Check if project is enable
-	if project.Project().State == "active" {
+	// Get perfdata
+	nbHost := len(hosts)
+	monitoringData.AddPerfdata("nbHost", nbHost, "")
 
-		// Get perfdata
-		nbHost := len(project.Hosts())
-		monitoringData.AddPerfdata("nbHost", nbHost, "")
-
-		// Check if project have hosts
-		if len(project.Hosts()) == 0 {
-			monitoringData.SetStatus(modelMonitoring.STATUS_WARNING)
-			monitoringData.AddMessage(fmt.Sprintf("Project '%s' have no host", projectName))
-			// Add perfdata to follow the host online
-			monitoringData.AddPerfdata("nbHostOnline", 0, "")
-			return monitoringData, nil
-		}
-
-		// We check the hosts state
-		nbHostOnline := 0
-		for _, host := range project.Hosts() {
-			if host.State == "inactive" {
-				monitoringData.AddMessage(fmt.Sprintf("Host '%s' is disabled", host.Name))
-			} else if host.State != "active" {
-				monitoringData.AddMessage(fmt.Sprintf("Host '%s' have problem", host.Name))
-				monitoringData.SetStatus(modelMonitoring.STATUS_CRITICAL)
-			} else {
-				nbHostOnline++
-			}
-		}
-
+	// Check if hosts is not empty
+	if len(hosts) == 0 {
+		monitoringData.SetStatus(modelMonitoring.STATUS_WARNING)
+		monitoringData.AddMessage(fmt.Sprintf("No host"))
 		// Add perfdata to follow the host online
-		monitoringData.AddPerfdata("nbHostOnline", nbHostOnline, "")
-
-		// All host work fine
-		if nbHostOnline == len(project.Hosts()) {
-			monitoringData.AddMessage(fmt.Sprintf("All hosts work fine (%d/%d hosts online)", nbHostOnline, len(project.Hosts())))
-		} else {
-			if nbHostOnline == 0 {
-				// Maybee all host are disabled
-				monitoringData.SetStatus(modelMonitoring.STATUS_WARNING)
-			}
-			monitoringData.AddMessage(fmt.Sprintf("Only %d/%d hosts online", nbHostOnline, len(project.Hosts())))
-		}
-
-	} else {
-		monitoringData.AddMessage(fmt.Sprintf("Project '%s' is not active. Current status is %s", projectName, project.Project().State))
+		monitoringData.AddPerfdata("nbHostOnline", 0, "")
 		return monitoringData, nil
+	}
+
+	// We check the hosts state
+	nbHostOnline := 0
+	for _, host := range hosts {
+		if host.State == "inactive" {
+			monitoringData.AddMessage(fmt.Sprintf("Host '%s' is disabled", host.Name))
+		} else if host.State != "active" {
+			monitoringData.AddMessage(fmt.Sprintf("Host '%s' have problem", host.Name))
+			monitoringData.SetStatus(modelMonitoring.STATUS_CRITICAL)
+		} else {
+			nbHostOnline++
+		}
+	}
+
+	// Add perfdata to follow the host online
+	monitoringData.AddPerfdata("nbHostOnline", nbHostOnline, "")
+
+	// All host work fine
+	if nbHostOnline == len(hosts) {
+		monitoringData.AddMessage(fmt.Sprintf("All hosts work fine (%d/%d hosts online)", nbHostOnline, len(hosts)))
+	} else {
+		if nbHostOnline == 0 {
+			// Maybee all host are disabled
+			monitoringData.SetStatus(modelMonitoring.STATUS_WARNING)
+		}
+		monitoringData.AddMessage(fmt.Sprintf("Only %d/%d hosts online", nbHostOnline, len(hosts)))
 	}
 
 	return monitoringData, nil
--- service/monitoring/host_test.go
+++ service/monitoring/host_test.go
@@ -1,120 +1,86 @@
 package monitoringService
 
 import (
-	"github.com/disaster37/check-rancher/model/project"
 	rancherClient "github.com/rancher/go-rancher/v2"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestCheckHostsProject(t *testing.T) {
+func TestCheckHosts(t *testing.T) {
 
-	// When project is nul
-	_, err := CheckHostsProject(nil)
+	// When hosts is null
+	_, err := CheckHosts(nil)
 	assert.Error(t, err)
 
-	// When project doesn't contains host
-	rancherProject := &rancherClient.Project{
-		Name:  "Test",
-		State: "active",
-	}
-	project, _ := modelProject.NewProject(rancherProject)
-	monitoringData, err := CheckHostsProject(project)
+	// When hosts list is empty
+	hosts := make([]rancherClient.Host, 0)
+	monitoringData, err := CheckHosts(hosts)
 	assert.Equal(t, 1, monitoringData.Status())
 	assert.Equal(t, 2, len(monitoringData.Perfdatas()))
 
-	// When project contains only host disabled
-	rancherProject = &rancherClient.Project{
-		Name:  "Test",
-		State: "active",
-	}
-	project, _ = modelProject.NewProject(rancherProject)
-	host := &rancherClient.Host{
+	// When contains only host disabled
+	hosts = make([]rancherClient.Host, 0)
+	host := rancherClient.Host{
 		Name:  "Test1",
 		State: "inactive",
 	}
-	project.AddHost(host)
-	monitoringData, err = CheckHostsProject(project)
+	hosts = append(hosts, host)
+	monitoringData, err = CheckHosts(hosts)
 	assert.Equal(t, 1, monitoringData.Status())
 	assert.Equal(t, 2, len(monitoringData.Perfdatas()))
 
 	// When all host work fine
-	rancherProject = &rancherClient.Project{
-		Name:  "Test",
-		State: "active",
-	}
-	project, _ = modelProject.NewProject(rancherProject)
-	host = &rancherClient.Host{
+	hosts = make([]rancherClient.Host, 0)
+	host = rancherClient.Host{
 		Name:  "Test1",
 		State: "active",
 	}
-	project.AddHost(host)
-	monitoringData, err = CheckHostsProject(project)
+	hosts = append(hosts, host)
+	monitoringData, err = CheckHosts(hosts)
 	assert.Equal(t, 0, monitoringData.Status())
 	assert.Equal(t, 2, len(monitoringData.Perfdatas()))
 
 	// When host has problem
-	rancherProject = &rancherClient.Project{
-		Name:  "Test",
-		State: "active",
-	}
-	project, _ = modelProject.NewProject(rancherProject)
-	host = &rancherClient.Host{
+	hosts = make([]rancherClient.Host, 0)
+	host = rancherClient.Host{
 		Name:  "Test1",
 		State: "disconnected",
 	}
-	project.AddHost(host)
-	monitoringData, err = CheckHostsProject(project)
+	hosts = append(hosts, host)
+	monitoringData, err = CheckHosts(hosts)
 	assert.Equal(t, 2, monitoringData.Status())
 	assert.Equal(t, 2, len(monitoringData.Perfdatas()))
 
 	// When host work fine with host disabled
-	rancherProject = &rancherClient.Project{
-		Name:  "Test",
-		State: "active",
-	}
-	project, _ = modelProject.NewProject(rancherProject)
-	host1 := &rancherClient.Host{
+	hosts = make([]rancherClient.Host, 0)
+	host1 := rancherClient.Host{
 		Name:  "Test1",
 		State: "inactive",
 	}
-	project.AddHost(host1)
-	host2 := &rancherClient.Host{
+	hosts = append(hosts, host1)
+	host2 := rancherClient.Host{
 		Name:  "Test1",
 		State: "active",
 	}
-	project.AddHost(host2)
-	monitoringData, err = CheckHostsProject(project)
+	hosts = append(hosts, host2)
+	monitoringData, err = CheckHosts(hosts)
 	assert.Equal(t, 0, monitoringData.Status())
 	assert.Equal(t, 2, len(monitoringData.Perfdatas()))
 
 	// When host has problem with host disabled
-	rancherProject = &rancherClient.Project{
-		Name:  "Test",
-		State: "active",
-	}
-	project, _ = modelProject.NewProject(rancherProject)
-	host1 = &rancherClient.Host{
+	hosts = make([]rancherClient.Host, 0)
+	host1 = rancherClient.Host{
 		Name:  "Test1",
 		State: "inactive",
 	}
-	project.AddHost(host1)
-	host2 = &rancherClient.Host{
+	hosts = append(hosts, host1)
+	host2 = rancherClient.Host{
 		Name:  "Test1",
 		State: "disconnected",
 	}
-	project.AddHost(host2)
-	monitoringData, err = CheckHostsProject(project)
+	hosts = append(hosts, host2)
+	monitoringData, err = CheckHosts(hosts)
 	assert.Equal(t, 2, monitoringData.Status())
 	assert.Equal(t, 2, len(monitoringData.Perfdatas()))
 
-	// When project is disabled
-	rancherProject = &rancherClient.Project{
-		Name:  "Test",
-		State: "inactive",
-	}
-	project, _ = modelProject.NewProject(rancherProject)
-	monitoringData, err = CheckHostsProject(project)
-	assert.Equal(t, 0, monitoringData.Status())
-	assert.Equal(t, 0, len(monitoringData.Perfdatas()))
 }
